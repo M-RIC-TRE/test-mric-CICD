a62dac3c242e9c6ed568273438cdc236b9ca14e4
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-03-21 12:28:47 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "RESOURCE_GROUP_NAME" {}
+variable "SQL_DB_NAME" {}
+variable "SQL_SERVER_NAME" {}
+
 variable "location" {
   type        = string
   description = "Location used for all resources"
