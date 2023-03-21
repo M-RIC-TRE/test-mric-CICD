457b6c6849f611d5122e08abae8634bd3e8c18a4
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-03-21 12:38:29 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "RESOURCE_GROUP_NAME" {}
-variable "SQL_DB_NAME" {}
-variable "SQL_SERVER_NAME" {}
+# variable "RESOURCE_GROUP_NAME" {}
+# variable "SQL_DB_NAME" {}
+# variable "SQL_SERVER_NAME" {}
 
 variable "location" {
   type        = string
