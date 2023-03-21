4f680f598a85573c7ca341a3cfcde2c9e71392f2
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-03-21 12:10:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,33 @@ resource "azurerm_resource_group" "rgfirst" {
   name     = "rg-mric-tf-001"  # var.resource_group -- will be environment variable
   location = var.location
 }
+
+resource "azurerm_sql_server" "sqlserver" {
+  name                         = "mric-tf-sqlsvr"
+  resource_group_name          = azurerm_resource_group.rgfirst.name
+  location                     = azurerm_resource_group.rgfirst.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_sql_database" "sqldb" {
+  name                             = "sample-db"
+  resource_group_name              = azurerm_resource_group.rgfirst.name
+  location                         = azurerm_resource_group.rgfirst.location
+  server_name                      = azurerm_sql_server.sqlserver.name
+  edition                          = "Basic"
+  collation                        = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode                      = "Default"
+  requested_service_objective_name = "Basic"
+}
+
+# Enables the "Allow Access to Azure services" box as described in the API docs
+# https://docs.microsoft.com/en-us/rest/api/sql/firewallrules/createorupdate
+resource "azurerm_sql_firewall_rule" "example" {
+  name                = "allow-azure-services"
+  resource_group_name = azurerm_resource_group.rgfirst.name
+  server_name         = azurerm_sql_server.sqlserver.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
