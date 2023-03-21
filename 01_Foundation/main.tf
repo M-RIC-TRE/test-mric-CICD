afd040113dceef653130edd55c2cb64b17e27241
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-03-21 15:54:28 +0000

    Update main.tf

--- 01_Foundation/main.tf
+++ 01_Foundation/main.tf
@@ -1,18 +1,18 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "rg-mgmt-terraform"
-    storage_account_name = "stmgmttfdev001"
-    container_name       = "tfcontain"
-    key                  = "tfcontain.tfstate"
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+#   backend "azurerm" {
+#     resource_group_name  = "rg-mgmt-terraform"
+#     storage_account_name = "stmgmttfdev001"
+#     container_name       = "tfcontain"
+#     key                  = "tfcontain.tfstate"
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
