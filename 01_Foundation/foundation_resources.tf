f4a5310b041159f955d082239991932ca577f67e
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-03-21 15:54:10 +0000

    Update foundation_resources.tf

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -1,6 +1,29 @@
 ### Data Sources ###
 data "azurerm_client_config" "current" {}
 
+
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-mgmt-terraform"
+    storage_account_name = "stmgmttfdev001"
+    container_name       = "tfcontain"
+    key                  = "tfcontain.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 ##################################################
 # FOUNDATIONAL RESOURCES                         #
 ##################################################
