77d8c95df9877d24037eee21a40b0d95119a17d7
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-03-21 17:05:16 +0000

    Update main.tf

--- 0_All/main.tf
+++ 0_All/main.tf
@@ -7,12 +7,6 @@ terraform {
       version = "=3.0.0"
     }
   }
-#   backend "azurerm" {
-#     resource_group_name  = "rg-mgmt-terraform"
-#     storage_account_name = "stmgmttfdev001"
-#     container_name       = "tfcontain"
-#     key                  = "tfcontain.tfstate"
-#   }
 }
 
 provider "azurerm" {
