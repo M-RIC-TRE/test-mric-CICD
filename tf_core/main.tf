eda6ee78b0f29ff33d1bb450b21017ea49b712b1
Slawomir <107872721+slawpast@users.noreply.github.com>
2023-04-06 18:39:39 +0100

    Update main.tf

--- tf_core/main.tf
+++ tf_core/main.tf
@@ -1,25 +1,9 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-terraform-state"
-#     storage_account_name = "stamrictfstatesdev"
-#     container_name       = "tfstate"
-#     key                  = "dev.tfstate"
-#   }
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=3.0.0"
-#     }
-#   }
-# }
- 
-provider "azurerm" {
-  features {}
-}
-
 terraform {
+  backend "azurerm" {
+    key                  = "key.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -27,6 +11,19 @@ terraform {
     }
   }
 }
+ 
+provider "azurerm" {
+  features {}
+}
+
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+# }
 
 #Create Resource Group
 resource "azurerm_resource_group" "rgfirst" {
